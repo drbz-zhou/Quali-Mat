e9de4997204a235559ad24a3f9842b16576b583a
drbz-zhou <bo.zhou.dfki@gmail.com>
2021-08-24 18:30:37 +0200

    Update script1_basic_training_subcategory.py

--- script1_basic_training_subcategory.py
+++ script1_basic_training_subcategory.py
@@ -26,7 +26,6 @@ tools.tf_setup_GPU()
 numClass = 47
 out_Session = 3
 plim = 13 # 7 for 6 people (64GB RAM), 13 for all 12 people (needs 128GB RAM), 2 for 1 person with quick test
-subcat = 1 #subcategory
 
 params = {'batch_size': 100, 'shuffle': True, 'n_classes': numClass}
 params['dim'] = (128, 64, 50)
@@ -63,9 +62,9 @@ for P in range(1,plim):
         datastr = 'P'+str(P)+'R'+str(R)
         mSliceDict[datastr]=np.genfromtxt(
             params['labelpath']+datastr+'_label_'+str(numClass)+'b.csv', delimiter=',',dtype=int)
-
-for subcat in range( 2, 3 ): #subcategory  (1, 10)
-    #%% leave session out
+#%%
+for subcat in range( 1, 10 ): #subcategory  (1, 10)
+    #% leave session out
     # select the training and testing indexes based on person, recording and class conditions
     #train_list = np.where( (Meta_Ind[:,1]!=out_Session) & (Meta_Ind[:,0]<plim) )[0].tolist()
     #test_list = np.where( (Meta_Ind[:,1]==out_Session)  & (Meta_Ind[:,0]<plim) )[0].tolist()
@@ -82,26 +81,13 @@ for subcat in range( 2, 3 ): #subcategory  (1, 10)
     test_list_ind = np.array(test_list)[test_subind].tolist()
     
     # for liminting classes of the subcategory
-    sub_classes = True
-    if sub_classes:
-        unique_labels=np.unique(Meta_Ind[ np.where((Meta_Ind[:,4]==1)) ,3])
-        numClass = len(unique_labels)
-        params['n_classes']=numClass
-        params['y_offset']=unique_labels.min()
+    unique_labels=np.unique(Meta_Ind[ np.where((Meta_Ind[:,4]==subcat)) ,3])
+    numClass = len(unique_labels)
+    params['n_classes']=numClass
+    params['y_offset']=unique_labels.min()
         
     tools.print_time()
-    #for i in range(10):
-    #    mDataExample=train_gen.__getitem__(i)
-    #    tools.print_time()
-    #%% check if the list is ok, especially if leave P or R out is properly assigned
-    # PRS = np.zeros((3,len(train_subind)))
-    # for i in range(len(train_subind)):
-    #     P, R, S = parser.decode_index_mem(train_subind[i], Meta_Ind)
-    #     PRS[:,i] = [P,R,S]
-    #%%
-    #mDataExample=train_gen.__data_generation([10])
-    #model = model_builder.build_Conv3D(filters=5, kernel=3, dense=256, numClass=numClass)
-    #model = model_builder.build_TConv_Incpt(filters = 5, kernel = (1,1,3), fine_tune_at = 500, numClass = numClass)
+    
     model_arch="Conv_Trans"
     if model_arch=="Conv3D":
         model = model_builder.build_Conv3D(
@@ -132,9 +118,8 @@ for subcat in range( 2, 3 ): #subcategory  (1, 10)
     val_loss = []
     epoch = 10000
     modelsavefile = '../Outputs/model_'+model_arch+'_'+'subcat_'+str(subcat)+'.h5'
-    patience= 50
+    patience= 40
     
-    #%%            
     train_gen = DataGenerator_mem(train_list_ind, datadict=mDataDict, Meta_Ind=Meta_Ind, slicedict=mSliceDict,**params)
     valid_gen = DataGenerator_mem(valid_list_ind, datadict=mDataDict, Meta_Ind=Meta_Ind, slicedict=mSliceDict, **params)
     # train model
@@ -146,21 +131,22 @@ for subcat in range( 2, 3 ): #subcategory  (1, 10)
     tools.print_time()
     #% test model
     model.load_weights(modelsavefile) #model needs to be built first 
-    params_test = params
+    params_test = params.copy()
     params_test['batch_size'] = 1
     params_test['shuffle'] = False
                 
-    test_gen = DataGenerator_mem(test_list_ind, datadict=mDataDict, Meta_Ind=Meta_Ind, slicedict=mSliceDict, **params)
-    m_y_test = labels[test_list_ind]-1
+    test_gen = DataGenerator_mem(test_list_ind, datadict=mDataDict, Meta_Ind=Meta_Ind, slicedict=mSliceDict, **params_test)
+    m_y_test = labels[test_list_ind]-params_test['y_offset']
     
     m_y_pred = model.predict(test_gen)
     acc_test = sum(m_y_test == np.argmax(m_y_pred, axis=1)) / m_y_test.shape[0]
     
     cm = confusion_matrix(m_y_test, np.argmax(m_y_pred, axis=1))
+    print('subcat_'+str(subcat))
     print(acc_test)
     print(cm)
     #%
-    tools.plot_confusion_matrix(cm, range(1, numClass+1), file_path = out_path + model_arch + '_' + \
+    tools.plot_confusion_matrix(cm, range(params['y_offset'], numClass+params['y_offset']), file_path = out_path + model_arch + '_' + \
             str(numClass) + '_LO' + str(out_Session)+'_'+'subcat_'+str(subcat))
     tools.plot_acc_loss(acc, val_acc, loss, val_loss, file_path = out_path + model_arch + '_' + \
             str(numClass) + '_LO' + str(out_Session)+'_'+'subcat_'+str(subcat))
